6dc04b58addab045706c3926a9424acf6d437be3
seldeclerck <seldeclerck@gmail>
2024-05-31 16:58:19 +0200

    Add create and stop spark

--- etl/spark_pi.py
+++ etl/spark_pi.py
@@ -5,4 +5,10 @@ from random import random
 from pyspark.sql import SparkSession
 
 if __name__ == "__main__":
-    print("starting spark job")
+    spark = SparkSession.builder.appName("PythonPi").getOrCreate()
+
+    spark.sparkContext.setLogLevel("DEBUG")
+
+    print(f"Pi is roughly 3.14")
+
+    spark.stop()
