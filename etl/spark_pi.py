06fe6ebb03a9d0199ae6c2aa247a09750927065b
seldeclerck <seldeclerck@gmail>
2024-05-31 17:02:13 +0200

    Add partiotions

--- etl/spark_pi.py
+++ etl/spark_pi.py
@@ -9,6 +9,10 @@ if __name__ == "__main__":
 
     spark.sparkContext.setLogLevel("DEBUG")
 
+    partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
+    n = 100000 * partitions
+
+    print(f"partitions is {n}")
     print(f"Pi is roughly 3.14")
 
     spark.stop()
